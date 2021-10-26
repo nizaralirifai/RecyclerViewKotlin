0dea9b547425f4d5d1ed40c6e782e92a5094f165
nizaralirifai <91579596+nizaralirifai@users.noreply.github.com>
2021-10-26 09:50:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewkotlin/Superhero.kt
@@ -0,0 +1,14 @@
+package com.example.recyclerviewkotlin
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Superhero(
+    val imgSuperhero: Int,
+    val nameSuperhero: String,
+    val descSuperhero: String,
+) : Parcelable
+
+
+
